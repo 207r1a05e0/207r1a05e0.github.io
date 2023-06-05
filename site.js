bea370b3574e22406a56e6e6480be0d96f0d3e56
207r1a05e0 <114235947+207r1a05e0@users.noreply.github.com>
2023-06-05 21:40:51 +0530

    performed first DOM manipulation

--- site.js
+++ site.js
@@ -1,1 +1,19 @@
-console.log("Hello kiran")
+var data1={image : "kiran1.jpg.jpeg", name : "Gummula Kiran Kumar" ,designation : "Software Engineer"};
+var data2={image : "kiran2.jpg.jpeg", name : "Kiran kumar" ,designation : "Software Developer"};
+
+var isdata1=true;
+var displaydata;
+var change_my_pick = function(){
+    if(isdata1)
+    {
+        displaydata=data2;
+        isdata1=false;
+    }
+    else
+    {
+        displaydata=data1;
+        isdata2=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+
+}
