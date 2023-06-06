1d438605538d353694a79943af071964f984f73c
207r1a05e0 <114235947+207r1a05e0@users.noreply.github.com>
2023-06-06 22:47:40 +0530

    update changes

--- site2.js
+++ site2.js
@@ -4,7 +4,6 @@ var changemypic=function(){
         userinfo.name=data.results[0].name.first;
         userinfo.image=data.results[0].picture.large;
         userinfo.username=data.results[0].login.username;
-
         document.getElementById("myimage").src=userinfo.image;
         document.getElementById("myname").innerHTML=userinfo.name;
         document.getElementById("mydesignation").innerHTML=userinfo.username;
